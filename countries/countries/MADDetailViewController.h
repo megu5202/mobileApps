e54aaf9ee78054bb27af1f0c5068ff5a0588ee79
Melissa <Melissa@rgnt2-87-170-dhcp.int.colorado.edu>
2013-10-24 10:44:49 -0600

    countries and scrabble

---
+++ countries/countries/MADDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADDetailViewController.h
+//  countries
+//
+//  Created by Melissa on 10/24/13.
+//  Copyright (c) 2013 Melissa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADDetailViewController : UITableViewController
+
+@end
---
+++ scrabbleQ/scrabbleQ/MADViewController.h
@@ -0,0 +1,13 @@
+//
+//  MADViewController.h
+//  scrabbleQ
+//
+//  Created by Melissa on 10/24/13.
+//  Copyright (c) 2013 Melissa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@end
