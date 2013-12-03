e872b6909ba4a176c38208055c9da052e3be95f8
Melissa <Melissa@rgnt2-86-195-dhcp.int.colorado.edu>
2013-12-03 10:27:51 -0700

    task lab done

---
+++ labs/task/task/MADViewController.h
@@ -0,0 +1,18 @@
+//
+//  MADViewController.h
+//  task
+//
+//  Created by Melissa on 12/3/13.
+//  Copyright (c) 2013 Melissa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *task1;
+@property (weak, nonatomic) IBOutlet UITextField *task2;
+@property (weak, nonatomic) IBOutlet UITextField *task3;
+@property (weak, nonatomic) IBOutlet UITextField *task4;
+
+@end
